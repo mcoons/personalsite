ecb974b1a5fd6a7d4ea811db5b7043c93d10ed63
mcoons <mcoons67@gmail.com>
2018-12-04 13:24:35 -0700

    added metatag for keywords

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || localport);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.locals.siteTitle = "Michael Coons";
+app.locals.siteTitle = "Michael Coons - Software Engineer Site";
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
